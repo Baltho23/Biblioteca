53e481074ba68bb340c37bec7f101885071f36eb
Airton <aldairton23@gmail.com>
2025-10-17 20:39:41 -0400

    correcion en el nombre de la tabla LOANS

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -8,7 +8,7 @@ INSERT INTO MEMBERS (name, email) VALUES
 ('Juan Perez', 'juan@example.com'),
 ('Laura Gomez', 'laura@example.com');
 
-INSERT INTO LOAN (book_id, member_id, loan_date, due_date, return_date) VALUES
+INSERT INTO LOANS (book_id, member_id, loan_date, due_date, return_date) VALUES
 (1, 1, DATE '2025-10-01', DATE '2025-10-10', DATE '2025-10-08'),  -- Devuelto a tiempo
 (2, 2, DATE '2025-10-05', DATE '2025-10-15', NULL),               -- Aún no devuelto
 (3, 1, DATE '2025-09-20', DATE '2025-09-30', DATE '2025-10-02');  -- Devuelto con retraso
